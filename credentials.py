fad2b002439751dfa1374475aa948a7756d8baa5
aiden-lee11 <138924727+aiden-lee11@users.noreply.github.com>
2024-01-24 12:59:11 -0600

    Add Credentials Template

---
+++ credentials.py
@@ -0,0 +1,6 @@
+class MyCreds:
+    def __init__(self) -> None:
+        self.client_id = "CLIENT ID HERE"
+        self.client_secret = "CLIENT SECRET HERE"
+        self.user_agent = "USER AGENT HERE"
+        self.path = "PATH TO CSV FILE HERE"
